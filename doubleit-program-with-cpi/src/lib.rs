0761d474f7428752d6fec31661d461ebc2b480d0
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-06-20 20:42:00 +0530

    double it program with cpi

--- doubleit-program-with-cpi/src/lib.rs
+++ doubleit-program-with-cpi/src/lib.rs
@@ -1,14 +1,41 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    instruction::{AccountMeta, Instruction},
+    program::invoke,
+    pubkey::Pubkey,
+};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+entrypoint!(process_instruction);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+fn process_instruction(
+    _public_key: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let mut iter = accounts.iter(); // Get iterator for accounts
+    // Extract data account and target contract address
+    let data_account = next_account_info(&mut iter)?;
+    let double_contract_address = next_account_info(&mut iter)?;
+    if !data_account.is_signer {
+        return ProgramResult::Err(
+            solana_program::program_error::ProgramError::MissingRequiredSignature,
+        );
     }
+
+    // Create instruction for cross-program invocation
+    let instruction = Instruction {
+        program_id: *double_contract_address.key,
+        accounts: vec![AccountMeta {
+            is_signer: true,
+            is_writable: true,
+            pubkey: *data_account.key,
+        }],
+        data: instruction_data.to_vec(),
+    };
+
+    // CPI - Invoke the target program with the instruction
+    invoke(&instruction, &[data_account.clone()])?;
+    Ok(())
 }
