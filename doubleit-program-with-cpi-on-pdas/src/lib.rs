d3b54542e6b52bf977f2fb0550655d1a9a8f7b95
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-06-21 23:35:51 +0530

    double it program with cpi and pdas program

--- doubleit-program-with-cpi-on-pdas/src/lib.rs
+++ doubleit-program-with-cpi-on-pdas/src/lib.rs
@@ -1,14 +1,33 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    instruction::{AccountMeta, Instruction},
+    program::invoke_signed,
+    pubkey::Pubkey,
+};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+entrypoint!(process_instruction);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn process_instruction(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let mut iter = accounts.iter();
+    let pda = next_account_info(&mut iter)?;
+    let user_pubkey = next_account_info(&mut iter)?;
+    let double_contract_program = next_account_info(&mut iter)?;
+
+    let instruction = Instruction {
+        program_id: *double_contract_program.key,
+        accounts: vec![AccountMeta::new(*pda.key, true)],
+        data: instruction_data.to_vec(),
+    };
+
+    let pda_seeds: &[&[u8]] = &[b"data_account", user_pubkey.key.as_ref()];
+
+    invoke_signed(&instruction, &[pda.clone()], &[&pda_seeds])?;
+
+    Ok(())
 }
